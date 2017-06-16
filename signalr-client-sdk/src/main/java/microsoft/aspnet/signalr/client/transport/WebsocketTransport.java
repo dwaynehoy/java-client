44a8d19948e0d5e9747b12ddfd4e46a0d05dbffc
Jakob Ogrin <jakob.ogrin@gmail.com>
2017-06-17 00:49:34 +0200

    fixes - remove forced /signalr in connection string and drops empty parameters

--- signalr-client-sdk/src/main/java/microsoft/aspnet/signalr/client/transport/WebsocketTransport.java
+++ signalr-client-sdk/src/main/java/microsoft/aspnet/signalr/client/transport/WebsocketTransport.java
@@ -69,15 +69,23 @@ public class WebsocketTransport extends HttpClientTransport {
         boolean isSsl = false;
         String url = null;
         try {
-            url = connection.getUrl() + "signalr/" + connectionString + '?'
+            url = connection.getUrl() + connectionString + '?'
                     + "connectionData=" + URLEncoder.encode(URLEncoder.encode(connectionData, "UTF-8"), "UTF-8")
-                    + "&connectionToken=" + URLEncoder.encode(URLEncoder.encode(connectionToken, "UTF-8"), "UTF-8")
-                    + "&groupsToken=" + URLEncoder.encode(groupsToken, "UTF-8")
-                    + "&messageId=" + URLEncoder.encode(messageId, "UTF-8")
-                    + "&transport=" + URLEncoder.encode(transport, "UTF-8");
+                    + "&connectionToken=" + URLEncoder.encode(URLEncoder.encode(connectionToken, "UTF-8"), "UTF-8");
+            if (!groupsToken.isEmpty()) {
+                    url+= "&groupsToken=" + URLEncoder.encode(groupsToken, "UTF-8");
+            }
+            if (!messageId.isEmpty()) {
+                    url += "&messageId=" + URLEncoder.encode(messageId, "UTF-8");
+            }
+            if (!transport.isEmpty()) {
+                    url += "&transport=" + URLEncoder.encode(transport, "UTF-8");
+            }
+
             if (connection.getQueryString() != null) {
                 url += "&" + connection.getQueryString();
             }
+
             if (url.startsWith("https://")) {
                 isSsl = true;
                 url = url.replace("https://", "wss://");
